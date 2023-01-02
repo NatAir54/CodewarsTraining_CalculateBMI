b51c9f3b591e2d960733687a061afb1a650b1fa5
Nataliia Koval <ruba.fish1500@gmail.com>
2023-01-02 15:42:38 +0200

    Initial commit

---
+++ src/bmi/calculatingBMI.java
@@ -0,0 +1,31 @@
+package bmi;
+
+/*
+* Write function bmi that calculates body mass index (bmi = weight / height2).
+*if bmi <= 18.5 return "Underweight"
+* if bmi <= 25.0 return "Normal"
+* if bmi <= 30.0 return "Overweight"
+* if bmi > 30 return "Obese"
+ */
+
+public class calculatingBMI {
+    public static void main(String[] args) {
+        System.out.println(bmi(57.0, 166.0));
+    }
+
+    public static String bmi(double weight, double height) {
+        double bmi = weight / Math.pow(height, 2);
+
+        if (bmi <= 18.5) {
+            return "Underweight";
+        }
+        if (bmi <= 25.0) {
+            return "Normal";
+        }
+        if (bmi <= 30.0) {
+            return "Overweight";
+        }
+
+        return "Obese";
+    }
+}
